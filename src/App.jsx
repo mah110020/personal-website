7ac607561a995c6f673aded8f9e4c6b70144dc57
Matt H <14212062+mah110020@users.noreply.github.com>
2019-08-24 18:04:16 -0500

    added parallax scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import Scene from "./Scene.jsx";
+import Parallax from "./Parallax.jsx";
 import "./App.scss";
 
 class App extends React.Component {
 	render() {
 		return (
-			<Scene radians={Math.PI/16} />
+			<div className="app">
+				<Parallax />
+			</div>
 		);
 	}
 }
---
+++ src/Parallax.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Scene from "./Scene.jsx";
+import "./Parallax.scss";
+
+class Parallax extends React.Component {
+	render() {
+		return (
+			<div className="parallax">
+
+				<div className="panel panel1">
+					<span>Hello</span>
+				</div>
+
+				<div className="slider">
+					<div className="panel panel2">
+						<Scene radians={Math.PI/16} />
+					</div>
+				</div>
+
+				<div className="panel panel3">
+					<span>Welcome</span>
+				</div>
+
+				<div className="panel panel4">
+					<span>Back</span>
+				</div>
+
+			</div>
+		);
+	}
+}
+
+export default Parallax;
