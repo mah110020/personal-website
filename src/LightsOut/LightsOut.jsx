79174c8d59f8574bd337b478fd971b25ed2276f7
Matt H <14212062+mah110020@users.noreply.github.com>
2019-10-25 00:17:49 -0500

    insert lights-out html into react iframe

--- src/LightsOut/LightsOut.jsx
+++ src/LightsOut/LightsOut.jsx
@@ -3,10 +3,24 @@ import "./LightsOut.scss";
 
 class LightsOut extends React.Component {
 
+	ref = React.createRef();
+
+	componentDidMount(){
+		this.forceUpdate();
+	}
+
 	render() {
+		const parentMin = !this.ref.current
+			? 0
+			: Math.min(this.ref.current.clientWidth, this.ref.current.clientHeight);
+
 		return (
-			<div className="lights-out">
-				Lights Out! A Game built entirely in CSS.
+			<div className="lights-out" ref={this.ref}>
+				<iframe
+					title="Lights Out!"
+					style={{width: `${parentMin}px`, height: `${parentMin}px`}}
+					src={process.env.PUBLIC_URL + '/LightsOut.html'}
+				/>
 			</div>
 		);
 	}
