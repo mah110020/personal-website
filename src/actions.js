81d16fab9551bc08769354ef2f1ef5866523ddfe
Matt H <14212062+mah110020@users.noreply.github.com>
2019-10-25 18:29:01 -0500

    no duplicate navigations

--- src/actions.js
+++ src/actions.js
@@ -9,7 +9,7 @@ export const NAVIGATE_URL = "NAVIGATE_URL";
  */
 
 export function navigateURL(url, push=true) {
-	if(push){
+	if(push && document.location.pathname !== url){
 		window.history.pushState(null, null, url);
 	}
 	return { type: NAVIGATE_URL, url };
