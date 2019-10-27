bcba505a3cd3bfdd1f851f3a31f9d2e2b0f1fa89
Matt H <14212062+mah110020@users.noreply.github.com>
2019-10-27 15:27:55 -0500

    add tabs to lights-out experiment, and add discussion section.

--- src/AboutMe/AboutMe.jsx
+++ src/AboutMe/AboutMe.jsx
@@ -7,14 +7,16 @@ class AboutMe extends React.Component {
 		return (
 			<div className="about-me">
 				<div className="text-column">
-					This is not a resume. This section is intended as a chance to get to know me a little.
-					<br/>
-					<br/>
-					Hi. I am Matthew Henderson. I currently work as a contractor for NASA Goddard.
-					NASA Goddard is located in Greenbelt, Maryland just outside the D.C. beltway.
-					I Initially moved to the D.C. area while my girlfriend Abby did graduate work
-					at the near-by university. I worked on-site for two years before moving to Texas
-					and continuing development for our projects in a remote capacity.
+					<p>
+						This is not a resume. This section is intended as a chance to get to know me a little.
+					</p>
+					<p>
+						Hi. I am Matthew Henderson. I currently work as a contractor for NASA Goddard.
+						NASA Goddard is located in Greenbelt, Maryland just outside the D.C. beltway.
+						I Initially moved to the D.C. area while my girlfriend Abby did graduate work
+						at the near-by university. I worked on-site for two years before moving to Texas
+						and continuing development for our projects in a remote capacity.
+					</p>
 				</div>
 			</div>
 		);
--- src/LightsOut/LightsOut.jsx
+++ src/LightsOut/LightsOut.jsx
@@ -1,14 +1,66 @@
 import React from "react";
+import {Tabs, Tab} from "../Tabs/Tabs.jsx";
 import "./LightsOut.scss";
+
+const lightsOutHTML = `${process.env.PUBLIC_URL}/LightsOut.html`;
+const lightsOutCSS = `${process.env.PUBLIC_URL}/LightsOut.css`;
 
 class LightsOut extends React.Component {
 	render() {
 		return (
 			<div className="lights-out">
-				<iframe
-					title="Lights Out!"
-					src={process.env.PUBLIC_URL + '/LightsOut.html'}
-				/>
+				<Tabs>
+					<Tab key="experiment" header="Lights Out!">
+						<iframe
+							title="Lights Out!"
+							src={lightsOutHTML}
+						/>
+					</Tab>
+					<Tab key="discussion" header="Discussion">
+						<div className="discussion-tab">
+							<div className="text-column">
+								<p>
+									This is an implementation of the game
+									{" "}
+									<span style={{fontStyle:"italic"}}>Lights&nbsp;Out!</span>
+									{" "}
+									intentionally built using only html and CSS.
+								</p>
+								<p>
+									No JavaScript is used. To prove this to you,
+									I have deliberately refrained from using JSX
+									or a CSS pre-processer, so that I may most easily
+									provide for your verification
+									{" "}
+									<a href={lightsOutHTML}>LightsOut.html</a>
+									{" "}
+									and
+									{" "}
+									<a href={lightsOutCSS}>LightsOut.css</a>
+									. These are instead mounted in this application
+									using an iframe. You can view the page source
+									of those documents.
+								</p>
+								<p>
+									State is controlled using hidden checkboxes.
+									User interaction changes checkbox state using
+									labels, and CSS selectors can determine how that state
+									translates back to visible representation.
+								</p>
+								<p>
+									The game begins in an initial
+									color configuration. Each time you click a
+									cell, the cell and its four adjacent cells
+									change colors. The objective of the game is
+									to make all the squares turn black. Afterwards
+									a message will appear congratulating the
+									player. But beware, this game can be difficult!
+								</p>
+							</div>
+						</div>
+					</Tab>
+				</Tabs>
+
 			</div>
 		);
 	}
