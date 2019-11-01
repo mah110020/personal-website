965414bb0048488da2f778eb9b80674a4fff3bc0
Matt H <14212062+mah110020@users.noreply.github.com>
2019-10-31 23:49:57 -0500

    handle serving production app

--- server/server.js
+++ server/server.js
@@ -1,8 +1,27 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
-app.get("/api/test", function (req, res) {
-	return res.send("This is a test.");
+app.get("/api/test", (req, res) => {
+	res.send("This is a test.");
 });
 
-app.listen(8080);
+// catch routes to files served as from the root url
+app.use("/", express.static(path.join(__dirname, "../build")));
+
+// failed API routes shouldn't serve the app
+app.get("/api/*", (req, res) => {
+	res.status(404).send("API not found.");
+});
+app.get("/api/", (req, res) => {
+	res.status(404).send("API not found.");
+});
+
+// catch-all: serve the web app.
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "../build/index.html"));
+});
+
+app.listen(8080, () => {
+	console.log("Server initialized.");
+});
