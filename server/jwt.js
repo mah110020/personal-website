5bf8a165659818fcd198769d0c43b8c9ec8de9f1
Matt H <14212062+mah110020@users.noreply.github.com>
2019-11-04 21:53:06 -0600

    added credential handling middleware with JWT and protected end-points.

---
+++ server/jwt.js
@@ -0,0 +1,48 @@
+const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
+
+const jwtSecret = crypto.randomBytes(64); // changes on server restarts
+const cookieName = "jwtAuthToken";
+
+const expiresIn = 24 * 3600; // [seconds] 1 day;
+
+const init = (req, res, next) => {
+
+	const login = payload => {
+		res.clearCookie(cookieName); // remove the auth cookie
+		const token = jwt.sign(payload, jwtSecret, {expiresIn}); // generate the token
+		res.cookie(cookieName, token); // set the auth cookie
+	};
+
+	req.login = login;
+
+	// relies on cookieParser middleware
+	const token = req.cookies[cookieName] || "";
+
+	// set payload null if user unauthorized
+	jwt.verify(token, jwtSecret, (err, payload) => {
+		if (err) { // throws error when invalid
+			req.payload = null; // falsy payload
+		} else {
+			req.payload = payload; // attach payload for handling
+		}
+	});
+
+	next();
+};
+
+const active = (req, res, next) => {
+
+	if( !req.payload ){
+		res.clearCookie(cookieName); // clear invalid cookie
+		next( new Error("Unauthorized.") );
+	} else {
+		next();
+	}
+
+};
+
+module.exports = {
+	init,
+	active
+};
--- server/server.js
+++ server/server.js
@@ -1,27 +1,47 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
+const cookieParser = require("cookie-parser");
+
+const auth = require("./jwt.js");
+
+const PORT = 8080;
+
 const app = express();
 
-app.get("/api/test", (req, res) => {
-	res.send("This is a test.");
+app.use(express.json()); // makes json request body available as object
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser()); // makes cookies available as object
+
+app.use(auth.init); // exposes `req.login` and `req.payload`; auth exposes middleware
+
+// intercept any unauthorized requests, send login page
+app.use((req, res, next) => {
+	const unauthorized = !req.payload;
+	if( unauthorized ){
+		res.set("Content-Type", "text/html");
+		res.send(new Buffer("<h2>You are an unauthorized user. TODO: Add a login page here.</h2>"));
+	} else {
+		next();
+	}
+});
+
+// failed API routes shouldn't serve the app
+app.get("/api/*", auth.active, (req, res) => {
+	res.status(404).send("API not found.");
+});
+app.get("/api/", auth.active, (req, res) => {
+	res.status(404).send("API not found.");
 });
 
 // catch routes to files served as from the root url
-app.use("/", express.static(path.join(__dirname, "../build")));
-
-// failed API routes shouldn't serve the app
-app.get("/api/*", (req, res) => {
-	res.status(404).send("API not found.");
-});
-app.get("/api/", (req, res) => {
-	res.status(404).send("API not found.");
-});
+app.use("/", [auth.active, express.static(path.join(__dirname, "../build"))]);
 
 // catch-all: serve the web app.
-app.get("*", (req, res) => {
+app.get("*", auth.active, (req, res) => {
 	res.sendFile(path.join(__dirname, "../build/index.html"));
 });
 
-app.listen(8080, () => {
-	console.log("Server initialized.");
+app.listen(PORT, () => {
+	console.log(`Server initialized on port ${PORT}.`);
 });
