52ac36b2710bb06170785a0530114015829795e1
mphender <14212062+mah110020@users.noreply.github.com>
2020-06-24 21:36:35 -0500

    disable site login authentication

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
+// login authentication has been disabled
+
 const express = require("express");
 const path = require("path");
-const fs = require("fs");
+//const fs = require("fs");
 const cookieParser = require("cookie-parser");
 
-const auth = require("./jwt.js");
-const {authenticate} = require("./authenticate.js");
+//const auth = require("./jwt.js");
+//const {authenticate} = require("./authenticate.js");
 
 const PORT = 8080;
 
@@ -14,10 +16,10 @@ app.use(express.json()); // makes json request body available as object
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser()); // makes cookies available as object
 
-app.use(auth.init); // exposes `req.login` and `req.payload`; auth exposes middleware
+//app.use(auth.init); // exposes `req.login` and `req.payload`; auth exposes middleware
 
 // api to login
-app.post("/", (req, res) => {
+/*app.post("/", (req, res) => {
 	const {token} = req.body;
 
 	// attempt login
@@ -32,10 +34,10 @@ app.post("/", (req, res) => {
 	} else { // not authorized
 		res.sendFile(path.join(__dirname, "./login.html"));
 	}
-});
+});*/
 
 // intercept any unauthorized requests, send login page
-app.use((req, res, next) => {
+/*app.use((req, res, next) => {
 	const unauthorized = !req.payload;
 	if( unauthorized ){
 		res.set("Content-Type", "text/html");
@@ -43,21 +45,21 @@ app.use((req, res, next) => {
 	} else {
 		next();
 	}
-});
+});*/
 
 // failed API routes shouldn't serve the app
-app.get("/api/*", auth.active, (req, res) => {
+app.get("/api/*"/*, auth.active*/, (req, res) => {
 	res.status(404).send("API not found.");
 });
-app.get("/api/", auth.active, (req, res) => {
+app.get("/api/"/*, auth.active*/, (req, res) => {
 	res.status(404).send("API not found.");
 });
 
 // catch routes to files served as from the root url
-app.use("/", [auth.active, express.static(path.join(__dirname, "../build"))]);
+app.use("/", [/*auth.active,*/ express.static(path.join(__dirname, "../build"))]);
 
 // catch-all: serve the web app.
-app.get("*", auth.active, (req, res) => {
+app.get("*"/*, auth.active*/, (req, res) => {
 	res.set("Content-Type", "text/html");
 	res.sendFile(path.join(__dirname, "../build/index.html"));
 });
